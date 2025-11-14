874fc733a7534117250aff6453635cf9b86f9a2d
Methodic404 <joaopaulomorimoli@gmail.com>
2025-11-14 11:16:18 -0300

    termianndo configuração do views app clientes

--- apps/clientes/views.py
+++ apps/clientes/views.py
@@ -3,3 +3,6 @@ from rest_framework import viewsets
 from .models import Cliente
 from .serializer import ClienteSerializer
 # Create your views here.
+class ClienteViewSet(viewsets.ModelViewSet):
+    queryset = Cliente.objects.all()
+    serializer_class = ClienteSerializer
