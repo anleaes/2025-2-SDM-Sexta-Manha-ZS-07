eaa3d188ab54d03d21c87096ba71dbb00ea5955d
Methodic404 <joaopaulomorimoli@gmail.com>
2025-11-14 11:06:08 -0300

    terminando configuração de models do app clientes

--- apps/clientes/models.py
+++ apps/clientes/models.py
@@ -5,3 +5,10 @@ class Category(models.Model):
     name = models.CharField('Nome', max_length=50)
     email = models.CharField('Email', max_length=100)
     senha = models.CharField('Senha', max_length=100)
+    class Meta:
+        verbose_name = 'Clientes'
+        verbose_name_plural = 'Clientes'
+        ordering =['id']
+
+    def __str__(self):
+        return f"{self.name} - {self.email}"
