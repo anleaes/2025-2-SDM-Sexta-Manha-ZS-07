5f059ee331cdde8c5e58d987b1cd58bf64cf4935
Methodic404 <joaopaulomorimoli@gmail.com>
2025-11-14 11:12:47 -0300

    terminando configuração do serializer do app clientes

--- apps/clientes/serializer.py
+++ apps/clientes/serializer.py
@@ -1,2 +1,7 @@
 from .models import Cliente
 from rest_framework import serializers
+
+class ClienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cliente
+        fields = '__all__'
