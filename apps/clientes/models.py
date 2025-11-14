d2073a0452bd7fc75b90ab4ca3133876fb8a6ab9
Methodic404 <joaopaulomorimoli@gmail.com>
2025-11-14 11:04:18 -0300

    configuração do models do app clientes

--- apps/clientes/models.py
+++ apps/clientes/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField('Nome', max_length=50)
+    email = models.CharField('Email', max_length=100)
+    senha = models.CharField('Senha', max_length=100)
