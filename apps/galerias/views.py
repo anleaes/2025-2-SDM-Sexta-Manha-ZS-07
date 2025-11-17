6eaf0f5824f18609ff90c928b360bca1e1b8ccd3
Methodic404 <joaopaulomorimoli@gmail.com>
2025-11-17 09:20:13 -0300

    terminando config do views galerias

--- apps/galerias/views.py
+++ apps/galerias/views.py
@@ -3,3 +3,7 @@ from rest_framework import viewsets
 from .models import Galeria
 from .serializer import GaleriaSerializer
 # Create your views here.
+
+class GaleriaViewSet(viewsets.ModelViewSet):
+    queryset = Galeria.objects.all()
+    serializer_class = GaleriaSerializer
