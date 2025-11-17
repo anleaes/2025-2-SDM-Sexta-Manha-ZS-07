ddf34a16d33f2bbca89cd5400af9acc63894f58f
Methodic404 <joaopaulomorimoli@gmail.com>
2025-11-17 09:11:38 -0300

    configurando models do app Galerias

--- apps/galerias/models.py
+++ apps/galerias/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Galeria(models.Model):
+    name = models.CharField('Nome', max_length=50)
+    endereco = models.CharField('Endereço', max_length=100)
+    capacidade = models.IntegerField('Capacidade')
+    
+    class Meta:
+        verbose_name = 'Galeria'
+        verbose_name_plural = 'Galerias'
+        ordering =['id']
+
+    def __str__(self):
+        return f"{self.name} - {self.endereco} - {self.capacidade}"
