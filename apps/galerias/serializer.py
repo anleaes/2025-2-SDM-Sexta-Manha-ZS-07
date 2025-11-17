7c6bd119ba1fcde5edbb416f736a590c751e19bf
Methodic404 <joaopaulomorimoli@gmail.com>
2025-11-17 09:17:34 -0300

    terminando config do serializer

--- apps/galerias/serializer.py
+++ apps/galerias/serializer.py
@@ -1,2 +1,7 @@
 from .models import Galeria
 from rest_framework import serializers
+
+class GaleriaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Galeria
+        fields = '__all__'
