a9a2c22f50dda88fe866780965c91650941a8ab7
Methodic404 <joaopaulomorimoli@gmail.com>
2025-11-18 09:20:09 -0300

    terminando views do app artistas

--- apps/artistas/views.py
+++ apps/artistas/views.py
@@ -3,3 +3,8 @@ from rest_framework import viewsets
 from .models import Artista
 from .serializer import ArtistaSerializer
 # Create your views here.
+
+
+class ArtistaViewSet(viewsets.ModelViewSet):
+    queryset = Artista.objects.all()
+    serializer_class = ArtistaSerializer
