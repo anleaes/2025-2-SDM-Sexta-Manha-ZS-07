ff821bac7f50a02eeaea2a80db1701c9d6af9061
Methodic404 <joaopaulomorimoli@gmail.com>
2025-11-18 09:11:05 -0300

    configuração do models do app artistas

--- apps/artistas/models.py
+++ apps/artistas/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Artista(models.Model):
+    name = models.CharField('Nome', max_length=50)
+    nacionalidade = models.CharField('Nacionalidade', max_length=30)
+    biografia = models.CharField('Biografia', max_length=500)
+    
+    class Meta:
+        verbose_name = 'Artista'
+        verbose_name_plural = 'Artistas'
+        ordering =['id']
+
+    def __str__(self):
+        return f"{self.name} - {self.nacionalidade} - {self.biografia}"
