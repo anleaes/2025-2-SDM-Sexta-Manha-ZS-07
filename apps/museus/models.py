e34ad11939b77912fc2686448fa3b1ad5bd716f4
Methodic404 <joaopaulomorimoli@gmail.com>
2025-11-19 09:25:33 -0300

    terminando config do models do app museus

--- apps/museus/models.py
+++ apps/museus/models.py
@@ -5,3 +5,11 @@ class Museu(models.Model):
     name = models.CharField('Nome', max_length=50)
     local = models.CharField('Local', max_length=100)
     horario = models.CharField('Horário de Funcionamento', max_length=100)
+
+    class Meta:
+        verbose_name = 'Museu'
+        verbose_name_plural = 'Museus'
+        ordering =['id']
+
+    def __str__(self):
+        return f"{self.name} - {self.local}"
