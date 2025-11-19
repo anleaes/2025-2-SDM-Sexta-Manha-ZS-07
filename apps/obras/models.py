cba791619e1297196cf32a42f5d3b6088d8039fa
Methodic404 <joaopaulomorimoli@gmail.com>
2025-11-19 14:16:45 -0300

    config final models app obras

--- apps/obras/models.py
+++ apps/obras/models.py
@@ -6,4 +6,10 @@ class Obra(models.Model):
     artista = models.CharField(max_length=100)
     status = models.CharField(max_length=50)
     localizacao = models.CharField(max_length=100)
+    class Meta:
+        verbose_name = 'Obra'
+        verbose_name_plural = 'Obras'
+        ordering =['id']
+    def __str__(self):
+        return f"{self.titulo} - {self.artista} - {self.status} - {self.localizacao}"
 
