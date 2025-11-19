cc03893fb268ff11bc7bb73884c0f2373a505b8c
Methodic404 <joaopaulomorimoli@gmail.com>
2025-11-19 14:31:38 -0300

    config final serializer app catalogos

--- apps/catalogos/serializer.py
+++ apps/catalogos/serializer.py
@@ -1,2 +1,7 @@
 from .models import Catalogo
 from rest_framework import serializers
+
+class CatalogoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Catalogo
+        fields = '__all__'
