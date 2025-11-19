5e25cf8a86509e9cbf1763eaa499623e7c1cb7d4
Methodic404 <joaopaulomorimoli@gmail.com>
2025-11-19 13:50:57 -0300

    config models app eventos

--- apps/eventos/models.py
+++ apps/eventos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    tema = models.CharField(max_length=100)
+    data_inicio = models.DateField()
+    data_fim = models.DateField()
