0f9af856bb09ee38f2c81735c0e28cfabe648374
Methodic404 <joaopaulomorimoli@gmail.com>
2025-11-19 14:28:58 -0300

    config incial models app catalogos

--- apps/catalogos/models.py
+++ apps/catalogos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Catalogo(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=255)
+
+   
