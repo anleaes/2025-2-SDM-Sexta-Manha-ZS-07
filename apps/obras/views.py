d4e188391fd6560087cd2a11876bb1202075405c
Methodic404 <joaopaulomorimoli@gmail.com>
2025-11-19 14:21:23 -0300

    config final views app obras

--- apps/obras/views.py
+++ apps/obras/views.py
@@ -3,3 +3,7 @@ from rest_framework import viewsets
 from .models import Obra
 from .serializer import ObraSerializer
 # Create your views here.
+
+class ObraViewSet(viewsets.ModelViewSet):
+    queryset = Obra.objects.all()
+    serializer_class = ObraSerializer
