0f6f197362a481e9c0baa8810b54fbd94f33b07a
Methodic404 <joaopaulomorimoli@gmail.com>
2025-11-19 14:16:10 -0300

    config incial models app obras

--- apps/obras/models.py
+++ apps/obras/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Obra(models.Model):
+    titulo = models.CharField(max_length=100)
+    artista = models.CharField(max_length=100)
+    status = models.CharField(max_length=50)
+    localizacao = models.CharField(max_length=100)
+
