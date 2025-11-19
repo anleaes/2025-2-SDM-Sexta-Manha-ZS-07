6ee5eae91c882617332440f989cb2c60b59818ac
Methodic404 <joaopaulomorimoli@gmail.com>
2025-11-19 09:24:32 -0300

    fazendo config inicial do models do app museus

--- apps/museus/models.py
+++ apps/museus/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Museu(models.Model):
+    name = models.CharField('Nome', max_length=50)
+    local = models.CharField('Local', max_length=100)
+    horario = models.CharField('Horário de Funcionamento', max_length=100)
