3bff8eb1bcc4f1b43469af257a54a593fdc52bca
Methodic404 <joaopaulomorimoli@gmail.com>
2025-11-19 14:19:05 -0300

    config final serializer app obras

--- apps/obras/serializer.py
+++ apps/obras/serializer.py
@@ -1,2 +1,7 @@
 from .models import Obra
 from rest_framework import serializers
+
+class ObraSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Obra
+        fields = '__all__'
