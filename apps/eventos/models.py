0193f865bbfa51d8027458e54bc858b06aa8aaa4
Methodic404 <joaopaulomorimoli@gmail.com>
2025-11-19 13:51:57 -0300

    config final models app eventos

--- apps/eventos/models.py
+++ apps/eventos/models.py
@@ -6,3 +6,10 @@ class Evento(models.Model):
     tema = models.CharField(max_length=100)
     data_inicio = models.DateField()
     data_fim = models.DateField()
+    class Meta:
+        verbose_name = 'Evento'
+        verbose_name_plural = 'Eventos'
+        ordering =['id']
+
+    def __str__(self):
+        return f"{self.titulo} - {self.tema} - {self.data_inicio} to {self.data_fim}"
